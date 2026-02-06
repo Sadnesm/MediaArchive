e56002b800606c53985e1cc213039912384f7442
Sadnesm <Blep@mail>
2026-02-06 21:30:58 +1000

    Add project files.

---
+++ MediaArchive/LibraryManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MediaArchive
+{
+    public class LibraryManager
+    {
+        public List<MediaItem> Items { get; set; } = new List<MediaItem>();
+
+        public void AddItem(MediaItem item) => Items.Add(item);
+
+        public void RemoveItem(Guid id) => Items.RemoveAll(i => i.Id == id);
+
+        // Поиск по подстроке
+        public List<MediaItem> Search(string query)
+        {
+            return Items.Where(i => i.Title.ToLower().Contains(query.ToLower()) ||
+                                    i.Author.ToLower().Contains(query.ToLower())).ToList();
+        }
+
+        // Фильтрация по жанру
+        public List<MediaItem> FilterByGenre(string genre)
+        {
+            if (genre == "Все") return Items;
+            return Items.Where(i => i.Genre == genre).ToList();
+        }
+    }
+}
---
+++ MediaArchive/MediaItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaArchive
+{
+    public class MediaItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
